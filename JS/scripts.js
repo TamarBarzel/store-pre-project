de3cfd19442bd26cb71ac8b8201a8cbb7f5ce70d
tamar <tamarlouf1@gmail.com>
2024-10-10 00:48:58 +0300

    side bar

---
+++ JS/scripts.js
@@ -0,0 +1,15 @@
+// sidebar.js
+document.addEventListener('DOMContentLoaded', function() {
+    const sidebar = document.getElementById('sidebar');
+
+    
+    const sidebarContent = `
+        <a href="#stationary">כלי כתיבה</a>
+        <a href="#notebooks">מחברות</a>
+        <a href="#colors">צבעים ויצירה</a>
+        <div class="devider"></div>
+        <a href="payment.html" class="payment"> לתשלום 🛒</a>
+    `;
+
+    sidebar.innerHTML = sidebarContent;
+});
