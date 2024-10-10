2b28aca68152d87142b12a82be23f4b05852065e
SariHop <sarihop12@gmail.com>
2024-10-10 10:26:18 +0300

    css and js footer

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,35 @@
 // sidebar.js
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.getElementById('sidebar');
 
-    
     const sidebarContent = `
         <a href="#stationary">כלי כתיבה</a>
         <a href="#notebooks">מחברות</a>
         <a href="#colors">צבעים ויצירה</a>
         <div class="devider"></div>
-        <a href="payment.html" class="payment"> לתשלום 🛒</a>
+        <a href="../HTML/payment-form.html" class="payment"> לתשלום 🛒</a>
     `;
 
     sidebar.innerHTML = sidebarContent;
 });
+
+// footer.js
+document.addEventListener('DOMContentLoaded', function () {
+    const footerDiv = document.getElementById('footer-container');
+
+    const footerContent = `
+       <footer>
+            <div class="footer-child">
+                <i class="fa fa-instagram"></i>
+                <i class="fa fa-facebook"></i>
+                <i class="fa fa-twitter"></i>
+            </div>
+            <div class="footer-child footer-Business-details" >
+                <a href="../HTML/Business-details.html" id='a-Business-details'>- תנאים וצור קשר -</a>
+            </div>
+        </footer>
+    `;
+
+    footerDiv.innerHTML = footerContent;
+});
+
